1f0351668e805666c0654d5347d7ed4f2c7608b3
AuroraBalestri <aurora.balestri76@gmail.com>
2022-10-18 17:36:53 +0200

    velocissimo design dell'applicativo

---
+++ LibraryAPI/Models/Customer.cs
@@ -0,0 +1,10 @@
+﻿namespace LibraryAPI.Models
+{
+    public class Customer
+    {
+        private int _customerId { get; set; }
+        private string _fullName { get; set; }
+        private string _address { get; set; }
+        private int _phoneNumbers { get; set; }
+    }
+}
